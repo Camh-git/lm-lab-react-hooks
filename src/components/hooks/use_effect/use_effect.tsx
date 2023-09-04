eba57ea0eeeef04a5281e67bf83f6ea9c196b76d
Camh-git <cameronhillier1@gmail.com>
2023-09-04 21:45:21 +0100

    Use effect update
    The use effect is mostly done. I used the fetch api and some state called data to get the data from the target url.
    For some reason setData isnt updating the data so I cant display it

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from "react";
+
 // make an api call using useEffect
+interface toDo {
+  completed: boolean;
+  userId: number;
+  id: number;
+  title: string;
+}
 
 export const APICall = () => {
+  // Your code here!
+  const [data, setData] = useState<toDo>();
 
-    // Your code here!
+  useEffect(() => {
+    const list = async () => {
+      const result = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      result.json().then((json) => {
+        console.log(json);
+        setData(json);
+        console.log(data);
+      });
+    };
+    list();
+  }, []);
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>Result:{}</p>
+    </>
+  );
+};
