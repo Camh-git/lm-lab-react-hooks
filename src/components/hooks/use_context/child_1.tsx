bbeb7cf9f390461ceee3142d85f313b9c5a80001
Camh-git <cameronhillier1@gmail.com>
2023-09-04 20:55:19 +0100

    Context first steps
    added a theme interface and a context, unsure how to make the context use the state and still be exportable at the same time.
    added a prototype for using the context to child 1.
    the hints and the videos are leading me in 2 different directions and its very confusing.

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -1,10 +1,13 @@
-export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
-	const theme = {
-		backgroundColor: dark ? '#333' : '#CCC',
-		color: dark ? 'white' : '#333',
-		padding: '2rem',
-		margin: '2rem',
-	};
+import { useContext } from "react";
 
-	return <div style={theme}>Child 1</div>;
+export const Child1: React.FC<{ dark: boolean }> = ({ dark }) => {
+  const theme = {
+    backgroundColor: dark ? "#333" : "#CCC",
+    color: dark ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
+  //const theme: <theme> = useContext{themeContext}
+
+  return <div style={theme}>Child 1</div>;
 };
--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,41 @@
-import { useState } from 'react';
-import { Child1 } from './child_1';
-import { Child2 } from './child_2';
+import { useState, createContext, useContext } from "react";
+import { Child1 } from "./child_1";
+import { Child2 } from "./child_2";
+
+const dark = false;
+interface theme {
+  backgroundColor: string;
+  color: string;
+  padding: string;
+  margin: string;
+}
+
+export const ThemeContext = createContext<theme>({
+  backgroundColor: dark ? "#333" : "#CCC",
+  color: dark ? "white" : "#333",
+  padding: "2rem",
+  margin: "2rem",
+});
 
 export const Parent = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
+  const ThemeContext = createContext<theme>({
+    backgroundColor: dark ? "#333" : "#CCC",
+    color: dark ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  });
 
-    const toggleTheme = () => {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
-
-    return (
-        <div className='section'>
-            <h2>useContext</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
-        </div>
-
-    )
-}
+  return (
+    <div className="section">
+      <h2>useContext</h2>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <Child1 dark={darkTheme} />
+      <Child2 dark={darkTheme} />
+    </div>
+  );
+};
