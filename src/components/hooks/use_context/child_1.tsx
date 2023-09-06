0da5d86ab4c3bfb624a3604481b693ecb295b65a
Camh-git <cameronhillier1@gmail.com>
2023-09-06 22:41:10 +0100

    revert as mentioned
    reverting the previous update, please see its commit message

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -1,10 +1,14 @@
 import { useContext } from "react";
-import { currentTheme } from "./themeObject";
-import { ThemeContext } from "./parent";
 
 export const Child1: React.FC = () => {
-  const theme = useContext(ThemeContext);
-  //const dark = false;
+  //const dark = useContext(currentTheme);
+  const dark = false;
+  const theme = {
+    backgroundColor: dark ? "#333" : "#CCC",
+    color: dark ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  };
 
   return <div style={theme}>Child 1</div>;
 };
--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -3,14 +3,14 @@ import { Child1 } from "./child_1";
 import { Child2 } from "./child_2";
 import { currentTheme, theme } from "./themeObject";
 
-export const ThemeContext = createContext<theme>(currentTheme);
-
 export const Parent = () => {
   const [darkTheme, setDarkTheme] = useState(true);
 
   const toggleTheme = () => {
     setDarkTheme((prevDarkTheme) => !prevDarkTheme);
   };
+
+  const ThemeContext = createContext<theme>(currentTheme);
 
   return (
     <div className="section">
