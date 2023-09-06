f89e04a8d4d47f3d7287f3aaf0f273db70cfdbc0
Camh-git <cameronhillier1@gmail.com>
2023-09-06 22:40:13 +0100

    confusing change
    I did what the tutorials said and imported the child directly in the childern, the values are now passing but the dark theme toggle has been cut out of the loop and no longer work.
    
    this is very confusing since if we have to manualy import the context in each child why not just import the values?
    also this approach cuts out the dark theme toggle since usestate cant be top level, but themecontext has to in order to export.
    will revert this, just wanted to post because it confuses me

--- src/components/hooks/use_context/child_1.tsx
+++ src/components/hooks/use_context/child_1.tsx
@@ -1,15 +1,10 @@
 import { useContext } from "react";
 import { currentTheme } from "./themeObject";
+import { ThemeContext } from "./parent";
 
 export const Child1: React.FC = () => {
-  //const dark = useContext(currentTheme);
-  const dark = false;
-  const theme = {
-    backgroundColor: dark ? "#333" : "#CCC",
-    color: dark ? "white" : "#333",
-    padding: "2rem",
-    margin: "2rem",
-  };
+  const theme = useContext(ThemeContext);
+  //const dark = false;
 
   return <div style={theme}>Child 1</div>;
 };
