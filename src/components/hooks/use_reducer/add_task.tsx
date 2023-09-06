40afb0bf7852e1a7903ec9a64a9593f34ac0ef3b
Camh-git <cameronhillier1@gmail.com>
2023-09-06 21:52:08 +0100

    Update add_task.tsx
    Made a few changes to reducer to decrease the number of errors it generates, since it is currently incomplete.

--- src/components/hooks/use_reducer/add_task.tsx
+++ src/components/hooks/use_reducer/add_task.tsx
@@ -1,12 +1,14 @@
 import { useState, useReducer } from "react";
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  return { state, action };
+}
 
 export const AddTask: React.FC<{ onAddTask: (text: string) => void }> = ({
   onAddTask,
 }) => {
   const [text, setText] = useState("");
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  //const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <div className="add-todo-container">
       <input
