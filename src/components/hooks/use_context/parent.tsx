f0aafa90f0226ef5751682b9f6a593a05878bfd7
Camh-git <cameronhillier1@gmail.com>
2023-09-07 22:28:59 +0100

    Finaly functional change context
    added a very verbose way of getting the state to change, based on ~5 tutorials, the docs, the recording and the slides.
    There is definitely a cleaner way to do this and i'd like to know what it is but this has been driving me round in circles and up the wall for the past 2 days.
    set the theme locally as shown in one of the slides, then used a verbose but certain to work set current theme function (trying to update it directly kept running into read-only errors and I couldn't figure out how to just change the 1st 2 values so I replace it with a new value based on.
    removed unnececary imports, exports and attributes from themeobject.
    updated child 2 to use the context.
    
    The next step would defiantly be to refactor this into something nicer, but I'm saving a working version first :)

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,15 +1,36 @@
 import { useState, createContext } from "react";
 import { Child1 } from "./child_1";
 import { Child2 } from "./child_2";
-import { currentTheme, ThemeContext } from "./themeObject";
+import { ThemeContext } from "./themeObject";
 
 export const Parent = () => {
   const [darkTheme, setDarkTheme] = useState(true);
 
+  const [currentTheme, setCurrentTheme] = useState({
+    backgroundColor: darkTheme ? "#333" : "#CCC",
+    color: darkTheme ? "white" : "#333",
+    padding: "2rem",
+    margin: "2rem",
+  });
+
   const toggleTheme = () => {
     setDarkTheme((prevDarkTheme) => !prevDarkTheme);
-    console.log("darkmode: " + darkTheme);
-    console.log("backcolour: " + currentTheme.backgroundColor);
+    setCurrentTheme(() => {
+      const newTheme = {
+        backgroundColor: "white",
+        color: "black",
+        padding: currentTheme.padding,
+        margin: currentTheme.margin,
+      };
+      if (darkTheme === true) {
+        newTheme.backgroundColor = "#ccc";
+        newTheme.color = "#333";
+      } else {
+        newTheme.backgroundColor = "#333";
+        newTheme.color = "white";
+      }
+      return newTheme;
+    });
   };
 
   return (
@@ -19,7 +40,7 @@ export const Parent = () => {
 
       <ThemeContext.Provider value={currentTheme}>
         <Child1 />
-        <Child2 dark={darkTheme} />
+        <Child2 />
       </ThemeContext.Provider>
     </div>
   );
--- src/components/hooks/use_context/themeObject.ts
+++ src/components/hooks/use_context/themeObject.ts
@@ -1,17 +1,15 @@
-import { useContext, createContext } from "react";
+import { createContext } from "react";
 export interface theme {
   backgroundColor: string;
   color: string;
   padding: string;
   margin: string;
-  darkMode: boolean;
 }
 const darkTheme = false;
-export const currentTheme: theme = {
+const defaultTheme: theme = {
   backgroundColor: darkTheme ? "#333" : "#CCC",
   color: darkTheme ? "white" : "#333",
   padding: "2rem",
   margin: "2rem",
-  darkMode: false,
 };
-export const ThemeContext = createContext<theme>(currentTheme);
+export const ThemeContext = createContext<theme>(defaultTheme);
